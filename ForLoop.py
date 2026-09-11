ec24acd30a02b326979a6478ad854bb4f5ef7d40
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-11 16:56:59 +1200

    Add ForLoop.py to print message multiple times

---
+++ ForLoop.py
@@ -0,0 +1,8 @@
+# ForLoop.py
+#
+
+user_input = int(input("Please enter the number of times "
+                       "that you wish to see the message."))
+
+for i in range(user_input):
+    print("hello world....", i)
