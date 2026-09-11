fdbead8403e25a878e5884685f2ebb8484a2ea17
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-11 17:07:45 +1200

    Comment out testing assertions
    Commented out assertion test code.

---
+++ ListsTask2.py
@@ -0,0 +1,12 @@
+# ListsTask2.py
+#
+
+days_of_week = ["Rahina","Ratu","Raapa","Rapare","Ramere","Rahoroi","Ratapu"]
+
+# Output:
+print(days_of_week[1],end=" ")
+print(days_of_week[4],end=" ")
+print(days_of_week[-3])
+
+# Testing
+# print("My assertions are:"\noutput = Ratu, Ramere, Ramere")
