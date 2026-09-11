f913734d12b5433e03c5c51cf6020911036854f9
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-11 16:44:03 +1200

    Add examples of primitive types in Python
    Added examples of various primitive types in Python including boolean, strings, lists, tuples, and dictionaries.

---
+++ PrimitiveTypes.py
@@ -0,0 +1,24 @@
+# PrimitiveTypes.py
+
+# Boolean:
+X = True
+Y = False
+print(X)
+print(Y)
+# You can check type of x and y by using type() method.
+
+# Strings:
+X = 'Python'
+Y = "Language"
+# You can check type of x and y by using type() method.
+
+# Lists:
+x = ["John", "Bob", "Sarah"]
+
+
+# Tuples:
+x = ("John", "Bob", "Sarah")
+
+# Dictionary:
+x = { "std1":"John", "std2":"Bob", "std3":"Sarah"}
+# Dictionary is stored as key values pairs e.g., std1 is key and John is value of it.
