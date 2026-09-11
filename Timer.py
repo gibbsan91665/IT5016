c6dec5ce234e42cae4ec3f263e1d3b102b30c5fd
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-11 17:29:27 +1200

    Add a simple timer that counts for a duration

---
+++ Timer.py
@@ -0,0 +1,16 @@
+# Timer.py
+#
+from datetime import datetime, timedelta
+
+duration = 5
+run = input("enter s to begin...")
+period = timedelta(seconds=1)
+next_time = datetime.now() + period
+
+counter = 0
+while run == 's' and counter < duration:
+    if next_time <= datetime.now():
+        print("..", counter)
+        counter += 1
+        # reevaluate the next_time variable
+        next_time += period
