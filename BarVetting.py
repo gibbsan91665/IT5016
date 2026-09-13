560b5b9295d47c9ed80cbc2f89585910c9668fac
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-14 03:11:20 +1200

    Implement patron vetting logic in BarVetting.py

---
+++ BarVetting.py
@@ -0,0 +1,12 @@
+# Boolean Comparisons
+# Challenge 1.2
+
+print("Welcome to the Bar\n")
+print("Please provide your details to vet the patrons")
+name = input('Enter your full name:')
+name=name.lower()
+age_in_years = int(input('Enter your year of birth i.e (1999)'))
+print(" You can enter in bar: ",
+name!="wiremu rangi" and
+name != "suzanne may" and
+age_in_years <2000, "\n")
