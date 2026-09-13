cc6d1c5ec3f4126fbe2973db4d57a1df705962f4
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-14 03:07:45 +1200

    Add SearchingStrings2.py for string analysis

---
+++ SearchingStrings2.py
@@ -0,0 +1,12 @@
+# String2.py
+# Searching Strings
+
+string_1 = "here is a translation: Haere mai ki konei means come here!"
+
+print("The number of times that k appears is {0}\n".format(string_1.count("k")))
+
+konei_endindex = string_1.find("konei") + len("konei")
+print("The end index position of konei is {0}\n".format(konei_endindex))
+
+here_position = string_1.find("here", konei_endindex, len(string_1))
+print("Looking for the string_1 here, anytime after konei...\n\nThe string_1 here appears at index position..{0}".format(here_position))
