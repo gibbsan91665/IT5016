823b922d0e0f407a5b8356005136139a95bbfa56
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-14 03:04:24 +1200

    Add function to show age 10 years ago

---
+++ SimpleSignatures1.py
@@ -0,0 +1,9 @@
+# Signatures1.py
+# Simple signatures 1
+
+def show_young_age(my_age):
+    print("My age 10 years ago was {0}.\n"
+          .format(my_age - 10))
+    
+# invoking function
+show_young_age(22)
