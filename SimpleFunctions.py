f59ffd75e0f6792b973594d105f358a523d625ed
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-14 02:54:40 +1200

    Create SimpleFunctions.py with greeting function
    Added a simple Python function to print a greeting.

---
+++ SimpleFunctions.py
@@ -0,0 +1,13 @@
+# SimpleFunctions.py
+# Simple functions
+
+def show_hello():
+    print("Hello there, this is my very first function!!!\n\n")
+
+print("Testing my first user defined function...\n\n")
+
+# invoking the function
+show_hello()
+
+# invoking the function again
+show_hello()
