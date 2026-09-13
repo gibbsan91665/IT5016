017aec0edc636ae9931772fbaae0a61ac5050c92
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-14 03:05:01 +1200

    Add show_young_age function to display age and name

---
+++ SimpleSignatures2.py
@@ -0,0 +1,10 @@
+# Signatures2.py
+# Simple signatures 2
+
+def show_young_age(my_age, my_name):
+    print("My age 10 years ago was {0}.\n"
+          "My name is {1}."
+          .format(my_age - 10, my_name))
+
+# invoking function
+show_young_age(22, "Tuheitia")
