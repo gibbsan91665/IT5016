5d669971051aa4c929128d90d788f258c1a4d52a
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-14 02:56:49 +1200

    Add show_hello function with parameter usage

---
+++ FunctionsWithParameters.py
@@ -0,0 +1,18 @@
+# Function2.py
+# Functions with parameters
+
+def show_hello(param):
+  print("Hello there, the number of times this "
+  "function is called is {0}!!!\n\n".format(param))
+
+# creating and setting a counter
+counter = 0
+print("Testing my second user defined function...\n\n")
+
+counter += 1
+# invoking the function
+show_hello(counter)
+
+counter += 1
+# invoking the function again
+show_hello(counter)
