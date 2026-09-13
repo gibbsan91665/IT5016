c9bfd19581a1cfc6a695cbbbbfe47d66bbeb970d
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-14 03:27:57 +1200

    Initialize list_to_print and fix indentation
    Fixes the list_to_print initialization and corrects the indentation.

---
+++ Challenge - AppendingLists.py
@@ -0,0 +1,16 @@
+# Python Lists
+# Challenge 1.3
+
+list_1 = [23,66,23,12]
+temp_list = []
+list_to_print[]
+
+for item in list_1:
+  if item in temp_list:
+      list_to_print.append(item)
+  else:
+    temp_list.append(item)
+print(list_to_print)
+
+# Test Case Assertions:
+# Assertion 1: "23"
