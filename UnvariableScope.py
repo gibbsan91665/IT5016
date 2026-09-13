67bd7ce41eb0ddfb5a97227baaa95e2203f22f43
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-14 03:01:46 +1200

    Fix score update logic in show_new_score function
    Corrected the score update logic and print statements in the function.

---
+++ UnvariableScope.py
@@ -0,0 +1,23 @@
+# Function4.py
+# Functions with un-variable scope
+
+score = 4
+
+def show_new_score():
+    score += 1
+    print("You got another point...\n"
+          "Your score is now {0}.\n"
+          .format(score))
+# some run code
+x = input("Your score is {0} points...\n\n"
+          "Hit any key to get another point. "
+          .format(score))
+# invoking the function
+show_new_score()
+# test case assertion
+#'''
+print("Test case assertion: the inital score is 4 "
+    "and it should become 5!!")
+
+show_new_score()
+#'''
