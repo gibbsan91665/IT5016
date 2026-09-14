cf3634660e9259924a51bad8d342a1af720c1080
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-14 21:39:11 +1200

    Add functions for greeting and adding numbers

---
+++ W4L1 FunctionsWithParameters.py
@@ -0,0 +1,19 @@
+# functions_with_parameters
+# week4 - Lab 1
+
+def greet(name):
+  print(f"Hello, {name}!")
+
+# Call the function with a parameter
+greet("Alice")
+greet("Bob")
+
+def add_numbers(a, b):
+  return a + b
+
+# Call the function with parameters
+result1 = add_numbers (5, 10)
+result2 = add_numbers (2.5, 4.5)
+
+print(result1)
+print(result2)
