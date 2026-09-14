751df11e483b164013c9c210a3ac7ec1a6ae5a42
gibbsan91665 <gibbsan91665@gmail.com>
2026-09-14 21:25:33 +1200

    Add list slicing functionality in InHalf.py
    Implement list slicing to get the first half of a list.

---
+++ InHalf.py
@@ -0,0 +1,11 @@
+# Slicing Lists
+# Challenge 1.2
+
+list_1 = [34, 123, 5, 77, 59, 2, 4]
+
+length = len(list_1)
+half = (int(length/2))
+print(list_1[0:half])
+
+# Test Case Assertion
+# Assertion 1: output = [34, 123, 5, 77]
